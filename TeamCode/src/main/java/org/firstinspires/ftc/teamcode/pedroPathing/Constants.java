cfd10846e947bffcf820ddc2ece5738617330407
Ian N <ivanbogdmail@gmail.com>
2025-10-18 16:34:10 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants();
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static MecanumConstants driveConstants = new MecanumConstants();
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .build();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.Command;
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -18,38 +19,12 @@ import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
 
 @Config
 public class DriveSubsystem extends StealthSubsystem {
-    private final DcMotorEx frontLeft;
-    private final DcMotorEx frontRight;
-    private final DcMotorEx backLeft;
-    private final DcMotorEx backRight;
 
-    private final IMU imu;
+    private final Follower follower;
     private double headingOffset = 0.0;
 
-    public DriveSubsystem(HardwareMap hardwareMap) {
-        frontLeft = hardwareMap.get(DcMotorEx.class, "leftFront");
-        frontRight = hardwareMap.get(DcMotorEx.class, "rightFront");
-        backLeft = hardwareMap.get(DcMotorEx.class, "leftRear");
-        backRight = hardwareMap.get(DcMotorEx.class, "rightRear");
-
-        frontRight.setDirection(DcMotorEx.Direction.REVERSE);
-        backRight.setDirection(DcMotorEx.Direction.REVERSE);
-
-        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-
-        imu = hardwareMap.get(IMU.class, "imu");
-
-        IMU.Parameters imuParameters = new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-                )
-        );
-
-        imu.initialize(imuParameters);
+    public DriveSubsystem(HardwareMap hardwareMap, Follower follower) {
+        this.follower = follower;
     }
 
     public void setHeading(double headingOffset) {
@@ -57,37 +32,20 @@ public class DriveSubsystem extends StealthSubsystem {
     }
 
     public double getHeading() {
-        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS) - headingOffset;
+        return follower.getHeading();
     }
 
-    public void resetHeading() {
-        headingOffset = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    public void resetHeading(){
+        try {
+            follower.getPoseTracker().getLocalizer().resetIMU();
+        }catch(InterruptedException e){
+            //empty block
+        }
     }
 
-    public void stop() {
-        frontLeft.setPower(0.0);
-        frontRight.setPower(0.0);
-        backLeft.setPower(0.0);
-        backRight.setPower(0.0);
-    }
 
     public void drive(double x, double y, double rot) {
-        double heading = getHeading();
-        double dx = x * Math.cos(-heading) - y * Math.sin(-heading);
-        double dy = x * Math.sin(-heading) + y * Math.cos(-heading);
-
-        dx *= 1.1; // Counteract imperfect strafing
-
-        double denominator = Math.max(Math.abs(dy) + Math.abs(dx) + Math.abs(rot), 1);
-        double frontLeftPower = (dy + dx + rot) / denominator;
-        double backLeftPower = (dy - dx + rot) / denominator;
-        double frontRightPower = (dy - dx - rot) / denominator;
-        double backRightPower = (dy + dx - rot) / denominator;
-
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
+        follower.setTeleOpDrive(x, y, rot,  false);
     }
 
     public Command driveTeleop(DoubleSupplier x, DoubleSupplier y, DoubleSupplier rot) {
