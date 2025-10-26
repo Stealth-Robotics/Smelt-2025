1054f2ba787c58a5321a24c700d3a2de1090c926
Ian N <ivanbogdmail@gmail.com>
2025-10-26 14:25:03 -0700

    Update ShooterSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -11,7 +12,6 @@ import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
 public class ShooterSubsystem extends StealthSubsystem {
     private final DcMotorEx shooterMotor;
     private final Servo hoodServo;
-
     public ShooterSubsystem(HardwareMap hardwareMap) {
         shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
         hoodServo = hardwareMap.get(Servo.class, "hoodServo");
@@ -23,4 +23,5 @@ public class ShooterSubsystem extends StealthSubsystem {
     public void setPosition(double position) {
         hoodServo.setPosition(position);
     }
+
 }
