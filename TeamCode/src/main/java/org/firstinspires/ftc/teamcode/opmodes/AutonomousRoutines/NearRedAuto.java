3a621db31d72b3608523e21b49b9b4535434a69e
Zachary Basham <attackchickens33@gmail.com>
2025-11-10 08:43:22 -0800

    Added a new Autonomous Routine.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousRoutines/NearRedAuto.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.opmodes.AutonomousRoutines;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+public class NearRedAuto extends DecodeAutos{
+    public PathChain GoToShoot;
+    public PathChain GoToIntake;
+    public PathChain Intake;
+    public PathChain GoToShoot2;
+    public PathChain GoOffLine;
+
+    public NearRedAuto(Follower follower) {
+        GoToShoot = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(56.000, 9.000), new Pose(59.300, 18.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(106))
+                .build();
+
+        GoToIntake = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(59.300, 18.000), new Pose(47.500, 36.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(106), Math.toRadians(0))
+                .build();
+
+        Intake = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(47.500, 36.000), new Pose(13.500, 36.000))
+                )
+                .setTangentHeadingInterpolation()
+                .addParametricCallback(0, () -> {
+                    follower.setMaxPower(.2);
+                    intake.start();
+                    belt.start();
+                })
+                .addParametricCallback(1, () -> {
+                    follower.setMaxPower(1);
+                    intake.stop();
+                    belt.stop();
+                })
+                .setReversed()
+                .build();
+
+        GoToShoot2 = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(13.500, 36.000), new Pose(59.300, 18.000))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(106))
+                .build();
+
+        GoOffLine = follower
+                .pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(59.300, 18.000), new Pose(56.090, 60.404))
+                )
+                .setTangentHeadingInterpolation()
+                .build();
+
+    }
+}
